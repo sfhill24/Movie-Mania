a59cdf10801a643c67c024949a26aa4f231ec2a6
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-12 13:22:17 -0400

    created user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,25 @@
-const {Model, Datatypes, Sequelize} = require ("sequelize");
+const { Model, Datatypes, Sequelize } = require("sequelize");
 
-const User = Sequelize.afterDefine("user")
+const User = Sequelize.define("user", {
+    id: {
+        type: Datatypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    username: {
+        type: Datatypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: Datatypes.STRING,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    }
+});
 
 
 
