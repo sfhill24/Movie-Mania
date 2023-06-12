164513cd5e4cff6e2e86197d4b7d60659ab709bf
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-12 13:00:19 -0400

    initial file setup with express, mysql, and sequelize. Tested DB and server conenctions to confirm they are working

---
+++ database/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS movie_mania_db;
+
+CREATE DATABASE movie_mania_db;
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const {Model, Datatypes, Sequelize} = require ("sequelize");
+
+const User = Sequelize.afterDefine("user")
+
+
+
+
+module.export = user;
