92ce9a2bb58f9ab3c6e5e086e8c96c9e333f8341
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-14 00:14:38 -0400

    fixed issue with homepage not being responsive

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="container-fluid ">
-      <div className="test">
-        <img className="jumbo-pic " src={require("../src/assets/Jumbo.jpg")} alt="jumbo-pic" />
-        
-        <div className="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-            <div className="input-group-append">
-              <button className="btn btn-outline-secondary" type="button">Button</button>
-            </div>
-            
-        </div>
+      <div className="jumbo-pic flex center">
+        <img className="logo" src={require("../src/assets/movie1.png")} alt="logo" />
       </div>
 
-      <div>
+      <div className="card-div">
+        <div className="flex center">
+          <div className="input-group mb-3" id="search-bar">
+            <input type="text" className="form-control " placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary a:hover" type="button" id="a:hover">Button</button>
+            </div>
+
+          </div>
+        </div>
         <div className="row">
           <div className="col">
             1 of 2
@@ -40,8 +41,8 @@ function App() {
           </div>
         </div>
       </div>
-      <footer>
-      <h2>Movie Mania Ⓒ 2023 <span><img className=" source-img" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"></img> </span></h2> 
+      <footer className="footer">
+        <h2>Movie Mania Ⓒ 2023 <span><img className=" source-img" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"></img> </span></h2>
       </footer>
     </div>
 
