7c52b4545485fc884386cfb1b7e1b099e26c9b59
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-13 22:18:09 -0400

    added styling to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit5555 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid ">
+      <div className="test">
+        <img className="jumbo-pic " src={require("../src/assets/Jumbo.jpg")} alt="jumbo-pic" />
+        
+        <div className="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button">Button</button>
+            </div>
+            
+        </div>
+      </div>
+
+      <div>
+        <div className="row">
+          <div className="col">
+            1 of 2
+          </div>
+          <div className="col">
+            2 of 2
+          </div>
+          <div className="col">
+            3 of 3
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            1 of 3
+          </div>
+          <div className="col">
+            2 of 3
+          </div>
+          <div className="col">
+            3 of 3
+          </div>
+        </div>
+      </div>
+      <footer>
+      <h2>Movie Mania Ⓒ 2023 <span><img className=" source-img" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"></img> </span></h2> 
+      </footer>
     </div>
+
+
   );
 }
 
