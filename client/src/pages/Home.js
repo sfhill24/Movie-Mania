cb4affe0355330801a6f4674694de9c5b979cbc5
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-14 23:03:15 -0400

    connected search butoon to endpoint

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,27 @@ import MovieCard from "../components/MovieCard";
 
 
 const Home = () => {
-    const [popularMovieData, setPopularMovieData] = useState(null);
+    const [movieData, setMovieData] = useState(null);
+    const [searchInput, setSearchInput] = useState("");
 
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTlkMjRmNmU3YTAzZmZjYTE1YWZiZGY4ZjRkM2QyOCIsInN1YiI6IjY0ODcyMzUxZDJiMjA5MDBhZDNkOGEzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PShhPcFP3C6aB0zDpylL8-OcPN_z8OsF-iLRYmkHQcI'
+            }
+        };
+
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${searchInput}&include_adult=false&language=en-US&page=1`, options)
+            .then(response => response.json())
+            .then(response => setMovieData(response))
+            .catch(err => console.error(err));
+    };
+
+
+    //Popular Movies
     useEffect(() => {
         const options = {
             method: 'GET',
@@ -17,15 +36,19 @@ const Home = () => {
 
         fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
             .then(response => response.json())
-            .then(response => setPopularMovieData(response))
+            .then(response => setMovieData(response))
             .catch(err => console.error(err));
     }, []);
 
-    if (popularMovieData == null) {
+    if (movieData == null) {
         return <div className="no-projects-message">Loading...</div>;
     }
 
-    const movieCards = popularMovieData.results.map((movie) => {
+    const movieCards = movieData.results.map((movie) => {
+        if (movie.poster_path == null) {
+            return (null)
+        }
+
         return (<div className="col-md-auto d-flex">
             <MovieCard imageUrl={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
         </div>);
@@ -39,9 +62,22 @@ const Home = () => {
             <div className="card-div">
                 <div className="flex center">
                     <div className="input-group mb-3" id="search-bar">
-                        <input type="text" className="form-control " placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+
+                        <input
+                            type="text"
+                            className="form-control "
+                            placeholder="Search"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                            value={searchInput}
+                            onChange={(e) => setSearchInput(e.target.value)}
+                        />
+
                         <div className="input-group-append">
-                            <button className="btn btn-outline-secondary a:hover" type="button" id="a:hover">Button</button>
+                            <button className="btn btn-outline-secondary a:hover"
+                                onClick={handleSearch}
+                                type="button"
+                                id="a:hover">Button</button>
                         </div>
                     </div>
                 </div>
