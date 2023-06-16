ffd410c974121307fa630fb1b064bb19a9bd0fd9
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-06-16 12:50:37 -0400

    updated code to use db url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,19 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    database: process.env.DB_NAME, 
-    host: process.env.HOST,
-    password: process.env.DB_PASSWORD,
-    username: process.env.DB_USER
-});
+const sequelize = null;
+
+if (process.env.JAWSDB_URL != null) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+}
+else {
+    sequelize = new Sequelize({
+        dialect: 'mysql',
+        database: process.env.DB_NAME,
+        host: process.env.HOST,
+        password: process.env.DB_PASSWORD,
+        username: process.env.DB_USER
+    });
+}
 
 module.exports = sequelize;
